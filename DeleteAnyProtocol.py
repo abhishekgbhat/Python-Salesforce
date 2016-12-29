fa655be445746a19663e2f7ea3e0bb93a709d777
Abhishek G. Bhat <abhishek.bhat@forteresearch.com>
2016-12-29 12:54:37 +0530

    Updated

--- DeleteAnyProtocol.py
+++ DeleteAnyProtocol.py
@@ -2,8 +2,10 @@ from simple_salesforce import Salesforce
 import requests
 var  = False 
 
-def deleteProtocols(protocolToDelete):
-        
+def deleteProtocols(protocolToDelete,protocolName):
+    filename = 'Protocol Name '+ protocolName +'.txt'
+    print filename
+    target = open(filename, 'w+')     
     print "Deleting Subject Visit Procedure Records for the Respective Protocol"
 
     SVPRecords = sf.query("SELECT Id,Protocol_Procedure__r.Protocol__c FROM Subject_Visit_Procedure__c where Protocol_Procedure__r.Protocol__c = '%s'"%protocolToDelete)
@@ -13,7 +15,11 @@ def deleteProtocols(protocolToDelete):
         idListSVP.append(record['Id'])
         print ""+record['Id']
         sf.Subject_Visit_Procedure__c.delete(record['Id'])
+        target.write("Records Deleted from Subject Visit Procedure")
+        target.write(record['Id']);
+        target.write("\n")
     print (len(SVPRecords))," Records Deleted from Subject Visit Procedure"
+    
 
     print "Deleting Subject Visit Records for the Respective Protocol"
 
@@ -24,7 +30,11 @@ def deleteProtocols(protocolToDelete):
         idListSV.append(record['Id'])
         print ""+record['Id']
         sf.Subject_Visit__c.delete(record['Id'])
+        target.write('Records Deleted from Subject Visit')
+        target.write(record['Id']);
+        target.write("\n")
     print (len(SVRecords))," Records Deleted from Subject Visit "
+    
 
     print "Deleting Subject Segment  Records for the Respective Protocol"
 
@@ -34,8 +44,12 @@ def deleteProtocols(protocolToDelete):
     for record in SSRecords:
         idListSS.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Subject Segment')
+        target.write(record['Id']);
+        target.write("\n")
         sf.Subject_Segment__c.delete(record['Id'])
     print (len(SSRecords))," Records Deleted from Subject Segment "
+    
 
     print "Deleting Visit Procedure Records for the Respective Protocol"
 
@@ -45,8 +59,12 @@ def deleteProtocols(protocolToDelete):
     for record in VPRecords:
         idListVPR.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Visit Procedure ')
+        target.write(record['Id']);
+        target.write("\n")
         sf.Visit_Procedure__c.delete(record['Id'])
-    print (len(VPRecords))," Records Deleted from Visit Procedure "        
+    print (len(VPRecords))," Records Deleted from Visit Procedure "
+    
 
     print "Deleting Schedule Procedure Records for the Respective Protocol"
 
@@ -56,8 +74,12 @@ def deleteProtocols(protocolToDelete):
     for record in SPRecords:
         idListSP.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Schedule Procedure ')
+        target.write(record['Id']);
+        target.write("\n")
         sf.Schedule_Procedure__c.delete(record['Id'])
     print (len(SPRecords))," Records Deleted from Schedule Procedure "
+    
 
     print "Deleting Schedule Visit Procedure Records for the Respective Protocol"
 
@@ -67,8 +89,11 @@ def deleteProtocols(protocolToDelete):
     for record in SVPPRecords:
         idListSVPP.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Schedule Visit Procedure ')
+        target.write(record['Id']);
+        target.write("\n")
         sf.Schedule_Visit_Procedure__c.delete(record['Id'])
-    print (len(SVPPRecords))," Records Deleted from Schedule Procedure "
+    print (len(SVPPRecords))," Records Deleted from Schedule Visit Procedure "
 
     print "Deleting Protocol Procedure Records for the Respective Protocol"
 
@@ -78,6 +103,8 @@ def deleteProtocols(protocolToDelete):
     for record in PPRecords:
         idListPP.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Schedule Visit Procedure ' +record['Id'])
+        target.write("\n")
         sf.Protocol_Procedure__c.delete(record['Id'])
     print (len(PPRecords))," Records Deleted from Protocol Procedure "
 
@@ -89,6 +116,8 @@ def deleteProtocols(protocolToDelete):
     for record in VRecords:
         idListV.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Visit '+record['Id'])
+        target.write("\n")
         sf.Visit__c.delete(record['Id'])
     print (len(VRecords))," Records Deleted from Visit"
 
@@ -100,6 +129,8 @@ def deleteProtocols(protocolToDelete):
     for record in SRecords:
         idListSR.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Schedule '+record['Id'])
+        target.write("\n")
         sf.Schedule__c.delete(record['Id'])
     print (len(SRecords))," Records Deleted from Schedule"
 
@@ -111,6 +142,8 @@ def deleteProtocols(protocolToDelete):
     for record in CCRecords:
         idListCC.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Calendar '+record['Id'])
+        target.write("\n")
         sf.Calendar_Cycle__c.delete(record['Id'])
     print (len(CCRecords))," Records Deleted from Calendar"
 
@@ -122,6 +155,8 @@ def deleteProtocols(protocolToDelete):
     for record in SegRecords:
         idListSeg.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Segment '+record['Id'])
+        target.write("\n")
         sf.Segment__c.delete(record['Id'])
     print (len(SegRecords))," Records Deleted from Segment"
 
@@ -133,6 +168,8 @@ def deleteProtocols(protocolToDelete):
     for record in PSRecords:
         idListSeg.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Protocol Subject '+record['Id'])
+        target.write("\n")
         sf.Protocol_Subject__c.delete(record['Id'])
     print (len(PSRecords))," Records Deleted from Protocol Subject"
 
@@ -144,6 +181,8 @@ def deleteProtocols(protocolToDelete):
     for record in financialRecords:
         idListFinancials.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Financials '+record['Id'])
+        target.write("\n")
         sf.Financials__c.delete(record['Id'])
     print (len(financialRecords))," Records Deleted from Financials"
 
@@ -155,6 +194,8 @@ def deleteProtocols(protocolToDelete):
     for record in budgetRecords:
         idListBudget.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Budgets '+record['Id'])
+        target.write("\n")
         sf.Budget__c.delete(record['Id'])
     print (len(budgetRecords))," Records Deleted from Budgets"
 
@@ -166,8 +207,10 @@ def deleteProtocols(protocolToDelete):
     for record in APRecords:
         idListAp.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Adhoc Payments '+record['Id'])
+        target.write("\n")
         sf.Adhoc_Payment__c.Delete(record['Id'])
-    print (len(APRecords))," Records Deleted from Protocol Subject"
+    print (len(APRecords))," Records Deleted from Adhoc Payments "
 
     print "Deleting Email Setup for the Respective Protocol"
 
@@ -177,19 +220,23 @@ def deleteProtocols(protocolToDelete):
     for record in ESRecords:
         idListES.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Email Setup '+record['Id'])
+        target.write("\n")
         sf.Email_Setup__c.delete(record['Id'])
     print (len(ESRecords))," Records Deleted from Email Setup"
 
     print "Deleting Treatments for the Respective Protocol"
 
-    TRecords = sf.query("SELECT Id FROM Treament__c where Protocol__c = '%s'"%protocolToDelete)
+    TRecords = sf.query("SELECT Id FROM Treatment__c where Protocol__c = '%s'"%protocolToDelete)
     TRecords = TRecords['records']
     idListT=[];
     for record in TRecords:
         idListT.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Treatment '+record['Id'])
+        target.write("\n")
         sf.Treatment__c.delete(record['Id'])
-    print (len(TRecords))," Records Deleted from Email Setup"
+    print (len(TRecords))," Records Deleted from Treatment "
 
     print "Deleting Web Protocol for the Respective Protocol"
 
@@ -199,11 +246,16 @@ def deleteProtocols(protocolToDelete):
     for record in WPRecords:
         idListWP.append(record['Id'])
         print ""+record['Id']
+        target.write('Records Deleted from Web Protocol '+record['Id'])
+        target.write("\n")
         sf.Web_Protocol__c.delete(record['Id'])
     print (len(WPRecords))," Records Deleted from Web Protocol"
-         
+
+    target.write('Records Deleted from Protocol '+protocolToDelete)
+    target.write("\n")    
     sf.Protocol__c.delete(protocolToDelete);
     print "Deleted All the Things Realted with Protocol ID = ",protocolToDelete
+    target.close()
     return
 
 #Credentials to connect to salesforce using the simple_salesforce library/api
@@ -211,12 +263,14 @@ sf = Salesforce(username='support@ctmssite.com.abhishek', password='Roopgiri91%'
 print('Connection Succesful')
 print "All the protocols present in the salesforce (Allegro)"
 
-#protocolRecords = sf.query("SELECT Id,Name FROM Protocol__c ORDER BY CreatedDate DESC")
-#protocolRecords = protocolRecords['records']
-#print(len(protocolRecords))
-#for record in protocolRecords:
+protocolRecords = sf.query("SELECT Id,Name FROM Protocol__c ORDER BY CreatedDate DESC LIMIT 1")
+protocolRecords = protocolRecords['records']
+print(len(protocolRecords))
+for record in protocolRecords:
      #print ""+record['Id']+"              "+record['Name']
-deleteProtocols('a1w41000000QJz3')                     
+     protocolId = record['Id']
+     protocolName = record['Name'] 
+     deleteProtocols(protocolId,protocolName)                     
 
 print "\n\n"
 print('Succesful')
